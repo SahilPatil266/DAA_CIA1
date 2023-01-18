7a32179c5a709443886f67050ac73e9afb7c5402
SahilPatil266 <122955388+SahilPatil266@users.noreply.github.com>
2023-01-18 10:42:09 +0530

    Add files via upload
    Question:
    1. The shortest path from source node to all possible nodes with PRIMS, KRISKALS, DIJKSTRA'S algorithms using C/C++/Python/Java. Give a justification for the choice of the programming language.
    
    2. Give a note of comparison regarding the performance of the algorithms. If you feel that any of the algorithms fail to find any of the required paths, give justification for the same.
    
    Solution:
    Prims and Kruskals algorithms can't be used for directed graphs. Whereas, Dijkstras works only for non-negative edges. Therefore neither of the given algorithms are suitable for the given problem. In this case, Djikstras can be used using the help of Bellman Ford algorithm, which specialises in these situation. Hence, I've written a code for Bellman Ford (Djikstras) to find the shortest source path.

---
+++ Sahil_DAA_CIA1.py
@@ -0,0 +1,38 @@
+def addEdge(u, v, w):
+	graph.append([u, v, w])
+
+def printArr(dist):
+	print("Vertex Distance from Source S")
+	for i in range(V):
+		print("{0}\t\t{1}".format(i, dist[i]))
+
+def BellmanFord(src):
+	dist = [float("Inf")] * V
+	dist[src] = 0
+
+	for _ in range(V - 1):
+		for u, v, w in graph:
+			if dist[u] != float("Inf") and dist[u] + w < dist[v]:
+				dist[v] = dist[u] + w
+
+	for u, v, w in graph:
+		if dist[u] != float("Inf") and dist[u] + w < dist[v]:
+			print("This graph contains negative weight cycle.")
+			return
+
+	printArr(dist)
+
+if __name__ == '__main__':
+    V=6 
+    graph=[]
+    addEdge(0, 1, 10)
+    addEdge(0, 5, 8)
+    addEdge(1, 3, 2)
+    addEdge(2, 1, 1)
+    addEdge(3, 2, -2)
+    addEdge(4, 1, -4)
+    addEdge(4, 3, -1)
+    addEdge(5, 4, 1)
+
+    BellmanFord(0)
+
